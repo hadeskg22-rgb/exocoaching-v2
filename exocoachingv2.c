35f810a5743162393f31989dd07a600b9b9432b2
hadeskg22-rgb <hadeskg@gmail.com>
2026-02-19 22:45:31 +0000

    suite2

--- exocoachingv2.c
+++ exocoachingv2.c
@@ -1,13 +1,13 @@
 
 #include <stdio.h>
 int main() {
-    int x,cpc,cpp,nbrc,sommep,j,i,total,sommoy;
+    int x,cpc,cpp,nbrc,sommep,j,i,total,sommoy,cptimp;
  float moy,pourcentage;
     do {
-        printf("Entrez votre age : ");
+        printf("Entrez un nombre : ");
         scanf("%d", &x);
-    } while (x <= 0 || x % 2 != 0);
-    sommep=0;
+    } while (x <= 0 || x % 2 == 0);
+
     nbrc=0;
     cpp=0;
     sommoy=0;
@@ -17,32 +17,42 @@ int main() {
         cpc=0;
         sommep=0;
         total++;
-        for(j=1;j<i;j++){
+        for(j=1;j<=i;j++){
             if (i%j==0){
             cpc++;
             sommep=sommep+j;
         }
+        }
         if(cpc>2){
             nbrc++;
         }
-     if (sommep==i){
+     if (sommep-i==i){
             cpp++;
             sommoy=sommoy+i;
 
      }
-     if(i % j != 0 && i % 7 == 0 && i % 3 = 0 ){
+     if (i % 2 != 0 && i % 7 == 0 && i % 3 == 0 ){
             cptimp++;
 
      }
 
     }
 
+if(cpp != 0)
+        moy = (float)sommoy / cpp;
+    else
+        moy = 0;
 
 
 
 
-}
-printf("%d",cpp);
+
+
+pourcentage= (float)nbrc / total * 100;
+printf("%f est la moyenne\n",moy);
+printf("%f est le pourcentage\n",pourcentage);
+printf("%d est le nombre de nomnre impaire multiple de 7 et divisible par 3",cptimp);
+
 
 
 
